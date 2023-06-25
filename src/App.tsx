d8eec0c53fb4ca99e0d24047fb5560b277de741a
StarCat <84149668+Star-Cat13@users.noreply.github.com>
2023-06-25 15:10:57 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,36 @@ function App() {
 
   return (
     <>
+      <img className='logo' src='https://threebearsmotel.com/wp-content/uploads/2021/06/sign2.jpg' />
       <div>
-        <h1> Three Bears Motel Lincoln, MT</h1>
+        <h1>Three Bears Motel Lincoln, MT</h1>
         <p className='address'>MT HWY 200 203 Main St Lincoln, Montana 59639</p>
         <p className='phoneNumber'>406-362-4355</p>
       </div>
 
       <div className="section">
-        <img className='motelFront' src='https://threebearsmotel.com/wp-content/uploads/2021/06/3bearsoutside.jpg' />
+        <img className='fullpg' src='https://threebearsmotel.com/wp-content/uploads/2021/06/3bearsoutside.jpg' />
       </div>
 
       <div className='section'>
-        <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/sign2.jpg' />
-        <h2>Welcome</h2>
-        <p>The Three Bears Motel welcomes you to our family owned establishment in the mountains of Montana!
+        <h2>Welcome to Three Bears Motel</h2>
+        <h2>Your Cozy Retreat in Lincoln, Montana!</h2>
+        <p className='info-text'>Discover the warmth and charm of Three Bears Motel, a small family-run establishment nestled in the heart of Lincoln, Montana. As your perfect haven for relaxation and adventure, our motel offers a delightful escape from the busyness of everyday life. Immerse yourself in the tranquil beauty of our surroundings and experience the genuine hospitality that sets us apart.</p>
 
-          The following are just some of the services we offer to make your stay the perfect mountain getaway! Find more details on our services page.</p>
+      </div >
+      <div className='section'>
         <div className='icons'>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/wifi-signal-480x480.png' />
             <h3>WIFI</h3>
             <p>Free WiFi available for our guests.</p>
           </div>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/dog-480x480.png' />
             <h3>PET FRIENDLY</h3>
             <p>We have pet friendly rooms upon request. </p>
           </div>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2022/03/tipi-480x480.png' />
             <h3>TIPI</h3>
             <p>Sleep under the stars in a spacious tipi. Learn more. </p>
@@ -40,11 +42,13 @@ function App() {
         </div>
       </div>
 
+
+
       <div className='section'>
         <h2>Rooms</h2>
         {/* add room gallery */}
         <h3>Spread out and relax in a King or Queen Bed. We also have a room with 3 Extra Long Twin Beds for the Singles!</h3>
-        <ul>
+        <ul className='room-features'>
           <li>Check-in is 3:00 pm and Check-out is 11:00 am. If you need an early Check-in or late Check-out please ask and we will do the best we can to accommodate your needs.</li>
           <li>Every room has a refrigerator, microwave, single serve coffee, and DISH TV.</li>
           <li>All rooms are NON-SMOKING.</li>
@@ -56,20 +60,21 @@ function App() {
       <div className='section'>
         <h2>EXPERIENCE LINCOLN MONTANA</h2>
         <div className='icons'>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/fishing-480x480.png' />
             <h3>FISHING</h3>
           </div>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/backpack-480x480.png' />
             <h3>HIKING</h3>
           </div>
-          <div className='iconTile'>
+          <div className='icon-tile'>
             <img src='https://threebearsmotel.com/wp-content/uploads/2021/06/snowmobile-480x480.png' />
             <h3>SNOWMOBILING</h3>
           </div>
-          <p>Go fishing along the Blackfoot River. Hike the Continental Divide Trail. Experience the hundreds of miles of snowmobile and ATV trails. Or simply pull up a chair outside and take in the fresh air!</p>
         </div>
+        <p className='info-text'>Go fishing along the Blackfoot River. Hike the Continental Divide Trail. Experience the hundreds of miles of snowmobile and ATV trails. Or simply pull up a chair outside and take in the fresh air!</p>
+
       </div>
 
       <div className='section'>
